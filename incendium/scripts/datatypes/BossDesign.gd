1caad82226c877fee7206a6a6dc0905c35a96f9c
bk-tn <me@bk-tn.com>
2017-01-31 12:18:03 +0100

    BossDesign class

---
+++ incendium/scripts/datatypes/BossDesign.gd
@@ -0,0 +1,17 @@
+
+extends Object
+
+var layers = [3,3,3,3]
+var bullettypes = [0,0,0,0]
+var regex = ".*"
+
+var base_size = 100
+var size_dropoff = 0.6
+
+var base_health = 80
+
+var base_rot_speed = 0.3
+var rot_speed_inc = - PI * 0.1
+
+var start_color = Color(0,0,1)
+var end_color = Color(0,1,0)
